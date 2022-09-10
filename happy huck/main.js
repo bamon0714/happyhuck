4f347c54814ed87476786e67b98dbe8fccf27d7b
bamon0714 <111403952+bamon0714@users.noreply.github.com>
2022-09-10 22:30:10 +0900

    Add files via upload

---
+++ happy huck/main.js
@@ -0,0 +1,8 @@
+$(function($){
+   $('.firstview').bgSwitcher({
+      images:['images/background1.jpg','images/background2.jpg','images/background3.jpg','images/background4.jpg','images/background5.jpg'],
+      interval: 500,
+      loop: true,
+   }) ;
+
+});
