ab6e6140d6936e1f0035c218cdfe99cfd0544051
bamon0714 <111403952+bamon0714@users.noreply.github.com>
2022-09-15 13:23:52 +0900

    Add files via upload

--- happy huck/js/main.js
+++ happy huck/js/main.js
@@ -5,4 +5,44 @@ $(function($){
       loop: true,
    }) ;
 
+  $(window).scroll(function ( ){
+   var targetElement = $('.fadein').offset().top;
+   var scroll = $(window).scrollTop();
+   var windowHeight = $(window).height();
+   if(scroll > targetElement - windowHeight + 200 ){
+      $('.fadein').css('opacity', '1');
+      $('.fadein').css('transform','translateY(0)');
+   }
+
+  });
+
+
+  $('.slider').slick({
+   arrows: true,
+   autoplay: true,
+   dots: true,
+   slidesToShow: 4,
+   slidesToScroll: 1,
+});
+
+
+
+
+  $(".model-button").moddal();
+
+  $('nav a[href^="#"').click(function(){
+    var adjust=0;
+    var speed=400;
+    var herf=$(this).attr("href");
+    var target=$(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - adjust;
+    $('body,html').animate({
+       scrollTop: position
+     } ,speed, 'swing');
+     return false;
+    });
+
+
+
+
 });
